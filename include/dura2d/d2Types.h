6010fba33bf8b889d65409bbedb64a7c98b1fe75
Leandro Peres <leandroperes@protonmail.com>
2024-01-07 20:06:24 -0300

    Create d2Types.h

---
+++ include/dura2d/d2Types.h
@@ -0,0 +1,11 @@
+#ifndef D2TYPES_H
+#define D2TYPES_H
+
+typedef signed char	int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+#endif //D2TYPES_H
