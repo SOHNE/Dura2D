925bb09b00e8eaa970e795604e7d1d25a969cc4c
Leandro Peres <leandroperes@protonmail.com>
2024-01-16 18:26:20 -0300

    Update hello_world.cpp

--- unit-test/tests/hello_world.cpp
+++ unit-test/tests/hello_world.cpp
@@ -1,16 +1,16 @@
-#include "dura2d/d2World.h"
-
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include "../doctest.h"
+
 #include <cstdio>
+#include "dura2d/dura2d.h"
 
 DOCTEST_TEST_CASE("hello world")
 {
     d2Vec2 gravity(0.0F, 9.8F);
-    auto *world = new d2World(gravity);
+    d2World world(gravity);
 
-    d2Body *pBody = world->CreateBody(d2CircleShape(45), {0, 0}, 10.0F);
+    d2Body *pBody = world.CreateBody(d2CircleShape(45), {0, 0}, 10.0F);
     pBody->angularVelocity = 10.0F;
 
     constexpr float timeStep = 1.F / 60.F;
@@ -27,13 +27,10 @@ DOCTEST_TEST_CASE("hello world")
         printf("X: %4.2f | Y: %4.2f | Angle: %4.2f\n", position.x, position.y, angle);
 
         // Instruct the world to perform a single step of simulation.
-        world->Update(timeStep);
+        world.Update(timeStep);
     }
-
 
     // ##### TESTS #####
     CHECK( (position.y < 0.01F) );
     CHECK( (angle > 9.0F) );
-
-    delete world;
 }
