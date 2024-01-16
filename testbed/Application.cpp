f6b7174a61587814290a06f5d2a46a5eeb772819
Leandro Peres <leandroperes@protonmail.com>
2024-01-16 18:49:21 -0300

    Adjusted testbed for the new refactoration

--- testbed/Application.cpp
+++ testbed/Application.cpp
@@ -9,18 +9,16 @@ Application::Setup()
     SetTargetFPS(60);
     _isRunning = true;
 
-    // create 1000 boxes
-    for (int i = 0; i < 500; ++i) {
-        d2Body *body = world->CreateBody(d2BoxShape(10.0f, 10.0f), {(float) GetRandomValue(0, screenWidth),
-                                                                    (float) GetRandomValue(0, screenHeight)}, 1.0f);
-        body->restitution = 0.0f;
+    // create a box in middle with 0 mass
+    {
+        d2Body *mBody = world->CreateBody(d2BoxShape(50.0f, 50.0f), {screenWidth / 2.0f, screenHeight / 2.0f}, 0.0f);
+        mBody->SetAngularVelocity(0.5f);
     }
 
     // create ground
     {
         d2Body *bGround = world->CreateBody(d2BoxShape(screenWidth, 10.0f), {screenWidth / 2.0f, screenHeight - 5.0f},
                                             0.0f);
-        bGround->restitution = 0.0f;
     }
 
     // Add walls
@@ -29,8 +27,14 @@ Application::Setup()
         d2Body *bRightWall = world->CreateBody(d2BoxShape(10.0f, screenHeight),
                                                {screenWidth - 5.0f, screenHeight / 2.0f},
                                                0.0f);
-        bLeftWall->restitution = 0.0f;
-        bRightWall->restitution = 0.0f;
+    }
+
+    // create 1000 boxes
+    for (int i = 0; i < 10; ++i)
+    {
+        d2Body *body = world->CreateBody(d2BoxShape(10.0f, 10.0f), {(float) GetRandomValue(0, screenWidth),
+                                                                    (float) GetRandomValue(0, screenHeight)}, 1.0f);
+        body->SetRestitution(0.2f);
     }
 }
 
@@ -47,15 +51,14 @@ Application::Run()
 void
 Application::Input()
 {
-    if (IsKeyPressed(KEY_ESCAPE)) {
-        _isRunning = false;
-        return;
-    }
-
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
         auto mousePos = GetMousePosition();
         d2Body *body = world->CreateBody(d2BoxShape(50.0f, 50.0f), {mousePos.x, mousePos.y}, 1.0f);
-        body->restitution = 0.0f;
+    }
+
+    if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+        auto mousePos = GetMousePosition();
+        d2Body *body = world->CreateBody(d2CircleShape(25.0f), {mousePos.x, mousePos.y}, 1.0f);
     }
 }
 
@@ -75,20 +78,20 @@ Application::Render()
     // draw body
     {
         auto bodies = world->GetBodies();
-        for (auto body = bodies; body != nullptr; body = body->next) {
+        for (auto &body = bodies; body; body = body->GetNext()) {
             DrawObject(body);
         }
     }
 
     // Draw FPS
     {
-        DrawFPS(10, 10);
+        DrawFPS(15, 10);
 
         auto frameTimeInMs = GetFrameTime() * 1000.0f;
-        DrawText(TextFormat("Frame Time: %.2f ms", frameTimeInMs), 10, 30, 10, BLACK);
+        DrawText(TextFormat("Frame Time: %.2f ms", frameTimeInMs), 15, 30, 10, BLACK);
 
         auto bodyCount = world->GetBodyCount();
-        DrawText(TextFormat("Body Count: %d", bodyCount), 10, 50, 10, BLACK);
+        DrawText(TextFormat("Body Count: %d", bodyCount), 15, 50, 10, BLACK);
     }
 
     EndDrawing();
@@ -103,21 +106,24 @@ Application::Destroy()
 void
 Application::DrawObject(const d2Body *body)
 {
-    d2Shape *shape = body->shape;
+    d2Shape *shape = body->GetShape();
+
+    Color color = body->IsAwake() ? MAROON : DARKGRAY;
+
     switch (shape->GetType()) {
         case POLYGON:
         case BOX: {
-            float rot = body->rotation * RAD2DEG;
+            float rot = body->GetRotation() * RAD2DEG;
             auto *box = dynamic_cast<d2BoxShape *>(shape);
-            DrawRectanglePro({body->position.x, body->position.y, box->width, box->height},
+            DrawRectanglePro({body->GetPosition().x, body->GetPosition().y, box->width, box->height},
                              {box->width / 2.0f, box->height / 2.0f}, rot,
-                             Fade(MAROON, 0.3f)
+                             color
                             );
             break;
         }
         case CIRCLE: {
             auto *circle = dynamic_cast<d2CircleShape *>(shape);
-            DrawCircle(body->position.x, body->position.y, circle->radius, Fade(MAROON, 0.3f));
+            DrawCircle(body->GetPosition().x, body->GetPosition().y, circle->radius, color);
             break;
         }
         default:
