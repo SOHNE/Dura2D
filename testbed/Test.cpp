a8cd2dc6de027064faccdad7d86e2ec74bd08782
Leandro Peres <leandroperes@protonmail.com>
2024-01-23 09:33:56 -0300

    Created test base class

---
+++ testbed/Test.cpp
@@ -0,0 +1,47 @@
+#include "Test.h"
+
+#include <iostream>
+
+Test::Test()
+{
+    world = new d2World((d2Vec2){0.0f, -9.81f});
+    world->SetDebugDraw(&g_draw);
+}
+
+Test::~Test()
+{
+    delete world;
+    world = NULL;
+}
+
+void
+Test::Step()
+{
+    world->Update(GetFrameTime());
+}
+
+void
+Test::Input()
+{
+
+}
+
+void
+Test::Render()
+{
+    ClearBackground((Color){45, 45, 48, 255});
+    world->DebugDraw();
+}
+
+TestEntry g_availableTests[MAX_TESTS] = { {nullptr} };
+int g_testCount = 0;
+
+int RegisterTest(const char* category, const char* name, TestCreateFcn* fcn)
+{
+    int index = g_testCount;
+    if (index >= MAX_TESTS) return -1;
+
+    g_availableTests[index] = {category, name, fcn };
+    ++g_testCount;
+    return index;
+}
---
+++ testbed/Test.h
@@ -0,0 +1,39 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "dura2d/dura2d.h"
+#include "draw.h"
+#include "raylib.h"
+
+class Test
+{
+public:
+    Test();
+
+    virtual ~Test();
+
+    virtual void Step();
+    virtual void Input();
+    virtual void Render();
+
+public:
+    d2World *world{nullptr};
+};
+
+typedef Test* TestCreateFcn();
+
+int RegisterTest(const char* category, const char* name, TestCreateFcn* fcn);
+
+//
+struct TestEntry
+{
+    const char* category;
+    const char* name;
+    TestCreateFcn* createFcn;
+};
+
+#define MAX_TESTS 256
+extern TestEntry g_availableTests[MAX_TESTS];
+extern int g_testCount;
+
+#endif //TEST_H
