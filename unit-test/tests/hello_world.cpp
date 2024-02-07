331864517d8fc1d8ac6ea64de2d82943f1240463
Leandro Peres <leandroperes@protonmail.com>
2024-02-07 18:04:51 -0300

    Minor unit-tests fixes

--- unit-test/tests/hello_world.cpp
+++ unit-test/tests/hello_world.cpp
@@ -7,12 +7,15 @@
 
 DOCTEST_TEST_CASE("hello world")
 {
-    d2Vec2 gravity(0.0F, 9.8F);
+    // Set up the world
+    d2Vec2 gravity(0.0F, -9.81F);
     d2World world(gravity);
 
-    d2Body *pBody = world.CreateBody(d2CircleShape(45), {0, 0}, 10.0F);
-    pBody->SetAngularVelocity(10.0F);
+    // Create a body
+    d2Body *pBody = world.CreateBody(d2CircleShape(45), (d2Vec2){0.F, 0.F}, 10.0F);
+    pBody->SetAngularVelocity(1.0F);
 
+    // also known as delta time, or the time between frames
     constexpr float timeStep = 1.F / 60.F;
 
     d2Vec2 position {};
@@ -22,15 +25,16 @@ DOCTEST_TEST_CASE("hello world")
     for (int32_t i = 0; i < 60; ++i)
     {
         // Print the position and angle of the body.
-        position = pBody->GetPosition();
-        angle = pBody->GetRotation();
-        printf("X: %4.2f | Y: %4.2f | Angle: %4.2f\n", position.x, position.y, angle);
+        position = pBody->GetPosition();    // in meters
+        angle = pBody->GetRotation();       // in radians
+        printf("%d - X: %4.2f | Y: %4.2f | Angle: %4.2f\n", i, position.x, position.y, angle);
 
         // Instruct the world to perform a single step of simulation.
-        world.Step(timeStep, 3);
+        world.Step(timeStep);
     }
 
     // ##### TESTS #####
-    CHECK( (position.y < 0.01F) );
-    CHECK( (angle > 9.0F) );
+    CHECK( ( position.x == 0.0F ) );
+    CHECK( ( position.y > 0.0F ) );
+    CHECK( ( angle > 0.0F ) );
 }
