468ac43a364ac28f3f45dfc37777fa4308ccd1d4
Leandro Peres <leandroperes@protonmail.com>
2024-03-25 09:55:30 -0300

    main loop refactoring

--- testbed/main.cpp
+++ testbed/main.cpp
@@ -5,37 +5,34 @@
 #include <emscripten/bind.h>
 #endif
 
-void MainLoop(void *);
+void
+MainLoop(void* arg) //void* arg __attribute__((unused)))
+{
+	auto *game = static_cast<Application*>(arg);
+	game->Input();
+	game->Update();
+	game->Render();
+
+#ifdef __EMSCRIPTEN__
+	if (!game->IsRunning())
+    {
+        emscripten_cancel_main_loop();
+    }
+#endif
+}
 
 int
 main(int argc, char **argv)
 {
-    Application game;
+    Application game { };
 
 #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop_arg(MainLoop, &game, 0, 1);
 #else
-    while (game.IsRunning())
-        MainLoop(&game);
+    do MainLoop(&game); while (game.IsRunning());
 #endif
 
     game.Destroy();
 
     return 0;
-}
-
-void
-MainLoop(void* arg) // void* arg __attribute__((unused))
-{
-    Application* game = static_cast<Application*>(arg);
-    game->Input();
-    game->Update();
-    game->Render();
-
-#ifdef __EMSCRIPTEN__
-    if (!game->IsRunning())
-    {
-        emscripten_cancel_main_loop();
-    }
-#endif
 }
