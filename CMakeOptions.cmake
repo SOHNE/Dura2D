5e57d6d06ca62755784f1e3a1c52b645690f4b14
Leandro Peres <leandroperes@protonmail.com>
2024-10-16 16:40:08 -0300

    CCache as dependent option for main project

--- CMakeOptions.cmake
+++ CMakeOptions.cmake
@@ -1,3 +1,5 @@
+include(CMakeDependentOption)
+
 #--------------------------------------------------------------------
 # Configuration Options
 #--------------------------------------------------------------------
@@ -8,4 +10,4 @@ option(DURA_BUILD_UNIT_TESTS "Build the Dura2D unit tests" ON)
 # Build Options
 #--------------------------------------------------------------------
 option(BUILD_SHARED_LIBS "Build Dura2D as a shared library" OFF)
-option(USE_CCACHE "Enable compiler cache that can drastically improve build times" ON)
+option(USE_CCACHE "Enable compiler cache that can drastically improve build times" $DURA_IS_MAIN})
