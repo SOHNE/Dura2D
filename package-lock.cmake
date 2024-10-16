d73f5f6c6f670d0f1e6d530bc1669ce66fd5ece3
Leandro Peres <leandroperes@protonmail.com>
2024-10-16 16:35:39 -0300

    Added package lock

---
+++ package-lock.cmake
@@ -0,0 +1,17 @@
+# CPM Package Lock
+# This file should be committed to version control
+
+# PackageProject.cmake
+CPMDeclarePackage(PackageProject.cmake
+  VERSION 1.12.0
+  GITHUB_REPOSITORY TheLartians/PackageProject.cmake
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
+# Ccache.cmake
+CPMDeclarePackage(Ccache.cmake
+  VERSION 1.2.5
+  GITHUB_REPOSITORY TheLartians/Ccache.cmake
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
