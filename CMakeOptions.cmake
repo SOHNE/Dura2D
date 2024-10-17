f6e4d897d065aa54c170980e42cb121f937b2667
Leandro Peres <leandroperes@protonmail.com>
2024-10-17 06:56:05 -0300

    Removed unused options

--- CMakeOptions.cmake
+++ CMakeOptions.cmake
@@ -1,13 +1,7 @@
 include(CMakeDependentOption)
 
 #--------------------------------------------------------------------
-# Configuration Options
-#--------------------------------------------------------------------
-option(DURA_BUILD_TESTBED "Build the Dura2D testbed" $DURA_IS_MAIN})
-option(DURA_BUILD_UNIT_TESTS "Build the Dura2D unit tests" ON)
-
-#--------------------------------------------------------------------
 # Build Options
 #--------------------------------------------------------------------
 option(BUILD_SHARED_LIBS "Build Dura2D as a shared library" OFF)
-option(USE_CCACHE "Enable compiler cache that can drastically improve build times" $DURA_IS_MAIN})
+option(USE_CCACHE "Enable compiler cache that can drastically improve build times" ${DURA_IS_MAIN})
